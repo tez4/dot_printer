6f84631874a8c6f53433b2ea8e3d16f79d226564
tez4 <joel.twann@bluewin.ch>
2023-07-26 21:07:56 +0200

    Added basic dot rendering

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ class Program:
         # self.screen_height = self.screen.get_height()
         # self.screen_width = self.screen.get_width()
 
-        pygame.display.set_caption("Bad Renderer")
+        pygame.display.set_caption("Dot Printer")
         icon = pygame.image.load("./assets/spaceship.png")
         pygame.display.set_icon(icon)
 
@@ -44,6 +44,20 @@ class Program:
             elif event.type == pygame.VIDEORESIZE:
                 self.screen_height = self.screen.get_height()
                 self.screen_width = self.screen.get_width()
+
+        self.screen.fill('#FFFFFF')
+        RADIUS = 20
+
+        circle_surfaces = []
+        for color, offset in zip([(0, 255, 255, 128), (255, 0, 255, 128), (255, 255, 0, 128)], [0, 20, 40]):
+            for i in range(5):
+                for j in range(7):
+                    circle_surface = pygame.Surface((2 * RADIUS, 2 * RADIUS), pygame.SRCALPHA)
+                    pygame.draw.circle(circle_surface, color, (RADIUS, RADIUS), RADIUS)
+                    circle_surfaces.append((circle_surface, (600 + offset + i * 60, 400 + j * 80)))
+
+        for circle_surface, position in circle_surfaces:
+            self.screen.blit(circle_surface, position)
 
         pygame_widgets.update(events)
         pygame.display.update()
