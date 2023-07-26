7444d2c9ebdf1038a4f2f51d3c066d621bf80b0c
tez4 <joel.twann@bluewin.ch>
2023-07-26 21:37:22 +0200

    Added basic offsetting of colors

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ class Program:
         on_setattr=attr.setters.validate)
     running: Any = attr.ib(default=True)
     screen: Any = attr.ib(default=None)
+    image: str = attr.ib(default="./assets/test_image_04.jpg")
 
     def __attrs_post_init__(self):
         pygame.init()
@@ -32,11 +33,12 @@ class Program:
         pygame.display.set_caption("Dot Printer")
         icon = pygame.image.load("./assets/spaceship.png")
         pygame.display.set_icon(icon)
+        image = Image.open(self.image)
 
         while self.running:
-            self.run_program_loop()
+            self.run_program_loop(image)
 
-    def run_program_loop(self):
+    def run_program_loop(self, image):
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
@@ -48,13 +50,19 @@ class Program:
         self.screen.fill('#FFFFFF')
         RADIUS = 20
 
+        print(image.width)
+
+        colors = [(0, 0, 0, 128), (0, 255, 255, 128), (255, 255, 0, 128), (255, 0, 255, 128)]
         circle_surfaces = []
-        for color, offset in zip([(0, 255, 255, 128), (255, 0, 255, 128), (255, 255, 0, 128)], [0, 20, 40]):
+        for color, index in zip(colors, [0, 1, 2, 3]):
             for i in range(5):
                 for j in range(7):
+                    angle = index * -0.3926875  # radiens
+                    x = i * np.cos(angle) - j * np.sin(angle)
+                    y = i * np.sin(angle) + j * np.cos(angle)
                     circle_surface = pygame.Surface((2 * RADIUS, 2 * RADIUS), pygame.SRCALPHA)
                     pygame.draw.circle(circle_surface, color, (RADIUS, RADIUS), RADIUS)
-                    circle_surfaces.append((circle_surface, (600 + offset + i * 60, 400 + j * 80)))
+                    circle_surfaces.append((circle_surface, (600 + x * 80, 400 + y * 80)))
 
         for circle_surface, position in circle_surfaces:
             self.screen.blit(circle_surface, position)
