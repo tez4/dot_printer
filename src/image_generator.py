673501355009fab6b9662a4ebe45fa1a39b2b745
tez4 <joel.twann@bluewin.ch>
2023-07-27 11:35:38 +0200

    Probably improved color mixing

--- src/image_generator.py
+++ src/image_generator.py
@@ -21,26 +21,36 @@ def draw_image(image):
     img = Image.new('RGB', (image.width, image.height), color='#FFFFFF')
 
     colors = {
-        'black': (0, 0, 0),
-        'cyan': (0, 255, 255),
-        'yellow': (255, 255, 0),
-        'magenta': (255, 0, 255)
+        'black': (10, 10, 10),
+        'cyan': (10, 245, 245),
+        'yellow': (245, 245, 10),
+        'magenta': (245, 10, 245)
     }
     circles = [(425, 600, 50, 'magenta'), (450, 600, 50, 'cyan'), (475, 600, 50, 'black'), (400, 600, 50, 'yellow')]
 
     for i in range(image.width):
         for j in range(image.height):
             for circle in circles:
-                if ((i - circle[0]) ** 2 + (j - circle[1]) ** 2) ** 0.5 < circle[2]:
-                    old_color = img.getpixel((i, j))
-                    new_color = colors[circle[3]]
-                    if old_color == (255, 255, 255):
-                        img.putpixel((i, j), new_color)
-                    else:
-                        mixed_color = tuple([int((min(x) + sum(x) / 2) / 2) for x in zip(old_color, new_color)])
-                        img.putpixel((i, j), mixed_color)
+                draw_circle(img, i, j, circle, colors)
 
     img.show()
+
+
+def draw_circle(img, i, j, circle, colors):
+    distance = ((i - circle[0]) ** 2 + (j - circle[1]) ** 2) ** 0.5
+    radius = circle[2]
+    if distance < radius:
+        old_color = img.getpixel((i, j))
+        distance_part = distance / radius
+        density = - distance_part ** 6 + 1
+        new_color = colors[circle[3]]
+        new_color = tuple([int(255 - ((255 - i) * density)) for i in new_color])
+
+        if old_color == (255, 255, 255):
+            img.putpixel((i, j), new_color)
+        else:
+            mixed_color = tuple([min(x) for x in zip(old_color, new_color)])
+            img.putpixel((i, j), mixed_color)
 
 
 if __name__ == "__main__":
